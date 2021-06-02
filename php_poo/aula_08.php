f2f88935faa741e34a3d0fb95e653abdd4c8ab1f
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-06-02 06:30:37 -0300

    Revisao aula_08

---
+++ php_poo/aula_08.php
@@ -0,0 +1,33 @@
+<?php
+// Constantes
+
+class Pessoa {
+	const nome = 'Tales';
+	
+	public function exibirNome() {
+		// self :: const referencia à constante da classe
+		echo self::nome;
+	}
+}
+
+$pessoa = new Pessoa();
+$pessoa->exibirNome();
+echo '<hr>';
+
+class Tales extends Pessoa {
+	const nome = 'Oliver';
+	
+	public function exibirNome() {
+		// parent :: const referencia à constante da classe pai
+		echo parent::nome;
+		echo ' ';
+		// self :: const referencia à constante da própria classe, mesmo que a classe pai tenha outras constantes
+		// Caso não tenha nenhuma constante no filho, ele irá mostrar a constante do pai
+		echo self::nome;
+	}
+}
+
+$tales = new Tales();
+$tales->exibirNome();
+
+?>
