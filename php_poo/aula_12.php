040d35776a599132f16d6c17e848884474aadf3c
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-06-02 07:26:39 -0300

    Revisao aula_12

---
+++ php_poo/aula_12.php
@@ -0,0 +1,40 @@
+<?php
+
+// Perceba que nos temos até aqui:
+// Dois arquivos e duas classes com o mesmo nome
+// O namespace vem justamente para diferenciar esses arquivos
+
+// Aqui damos o require nos dois arquivos
+require 'classes/produto.php';
+require 'modulos/produto.php';
+
+// Se fizermos a instância do modo anterior
+// $produto = new Produto();
+// O Sistema retorna erro pois possuem dois arquivos com o mesmo nome
+// Então, tem que ser específico em relação a qual arquivo você quer
+
+$produto = new \classes\Produto();
+$produto->mostrarDetalhes();
+
+echo '<hr>';
+
+$produto_c = new \modulos\Produto();
+$produto_c->mostrarDetalhes();
+
+echo '<hr>';
+
+// Outra forma de fazer é utilizando o use
+// use pasta\arquivo;
+// Utilizando o use, você pode apelidar o arquivo
+
+use modulos\Produto as productModel;
+use classes\Produto as productClass;
+
+$produto_a = new productModel();
+$produto_a->mostrarDetalhes();
+
+echo '<hr>';
+
+$produto_b = new productClass();
+$produto_b->mostrarDetalhes();
+?>
