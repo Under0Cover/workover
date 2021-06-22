7ee1a52ecd8e401bc4274809dbe114835b404966
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-06-21 22:26:59 -0300

    Criando um composer.json na unha

---
+++ php_poo/composer_na_unha/aula_20.php
@@ -0,0 +1,5 @@
+<?php
+
+echo 'Criando o composer.json na unha....'
+
+?>
---
+++ php_poo/composer_na_unha/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4f05f28696047694a9136cda31459b18851c2d9d',
+    'name' => 'oliver/workover',
+  ),
+  'versions' => 
+  array (
+    'oliver/workover' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4f05f28696047694a9136cda31459b18851c2d9d',
+    ),
+  ),
+);
