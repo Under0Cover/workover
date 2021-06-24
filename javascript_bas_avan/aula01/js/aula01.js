de0bc30158186290ed041afa921a1000fe24dca3
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-06-24 19:29:58 -0300

    aula01 JS

---
+++ javascript_bas_avan/aula01/js/aula01.js
@@ -0,0 +1,14 @@
+var x  = 5;
+
+function primeiraFuncao() {
+    return x + 10;
+}
+
+
+alert(primeiraFuncao());
+
+function segundaFuncao(x, y) {
+    return x + y;
+}
+
+console.log(segundaFuncao(10, 10))
