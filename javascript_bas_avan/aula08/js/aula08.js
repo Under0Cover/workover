d6fbf5c915e8c6f33b80821257bb70ec3b8dd9f0
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-05 23:23:50 -0300

    aula 08

---
+++ javascript_bas_avan/aula08/js/aula08.js
@@ -0,0 +1,13 @@
+var numero = 1;
+var booleano;
+
+function verifica() {
+    if (numero === 1) {
+        booleano = true;
+    } else {
+        booleano = false;
+    }
+    console.log(booleano);
+}
+
+verifica();
