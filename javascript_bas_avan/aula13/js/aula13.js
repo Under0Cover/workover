5e6bbf0b7da30529eb1c38aac54cb1e4b801328b
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-06 23:15:37 -0300

    aula 13

---
+++ javascript_bas_avan/aula13/js/aula13.js
@@ -0,0 +1,21 @@
+function valor(x) {
+    switch(x) {
+        case 1:
+            console.log('O número é igual a 1');
+            break;
+        case 2:
+            console.log('O número é igual a 2');
+            break;
+        case 3:
+            console.log('O número é igual a 3');
+            break;
+        case 4:
+            console.log('O número é igual a 4');
+            break;
+        default:
+            console.log('O número é maior ou igual a 5');
+            break;
+    }
+}
+
+valor(2);
