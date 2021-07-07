1298ce666033b4b1ed03a2b842cd5f843b68dd39
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-06 21:23:32 -0300

    aula 10

---
+++ javascript_bas_avan/aula10/js/aula10.js
@@ -0,0 +1,24 @@
+var numero = 1;
+
+function exemplo() {
+    return numero;
+}
+
+console.log('Primeiro valor:');
+console.log(exemplo());
+
+function exemplo2() {
+    var numero = 5;
+    return numero + numero;
+}
+
+console.log('Segundo valor:');
+console.log(exemplo2());
+
+function exemplo3() {
+    var numero3 = 5;
+    return numero + numero3;
+}
+
+console.log('Terceiro valor:');
+console.log(exemplo3());
