5fd4e9d2f13d031647f5662c3fdcc0cf2d23aa9c
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-12 23:06:43 -0300

    aula 06

---
+++ js_completo/aula06/js/aula06.js
@@ -0,0 +1,10 @@
+console.log(document.createElement('div'));
+console.log(document.createElement('p'));
+
+var img = document.createElement('img');
+img.src = 'http://lorempixel.com/400/200/';
+console.log(img);
+
+var para = document.createElement('p');
+para.innerHTML = 'Estou sendo criado aqui...'
+console.log(para);
