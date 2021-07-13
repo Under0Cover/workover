92c54b60617edb93b5095597878352992328846e
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-13 15:59:24 -0300

    aula 07

---
+++ js_completo/aula07/js/aula07.js
@@ -0,0 +1,8 @@
+var versa = document.createElement('p');
+versa.innerHTML = 'Versa';
+
+var sentra = document.createElement('p');
+sentra.innerHTML = 'Sentra';
+
+document.getElementById('nissan').appendChild(versa);
+document.getElementById('nissan').appendChild(sentra);
