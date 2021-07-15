19427f2627f66152b6aafacbb0f1f57007ef444a
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-15 20:22:10 -0300

    aula 10

---
+++ js_completo/aula10/js/aula10.js
@@ -0,0 +1,26 @@
+var nome = 'Keven';
+//console.log(nome);
+
+//nome = 'Outro Keven'
+//console.log(nome);
+
+function testando () {
+    nome = 'Rafael';
+    var sobrenome = 'Anjos';
+    idade = 35;
+    let sexo = 'M';
+}
+
+console.log(nome);
+//console.log(sobrenome);
+//console.log(idade);
+//console.log(sexo);
+testando();
+console.log(nome);
+console.log(idade);
+//console.log(sexo);
+
+const casado = false;
+console.log(casado);
+//casado = true;
+//console.log(casado);
