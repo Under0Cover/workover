4a2f06cace6eaa23348f303608b34bd892387dc0
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-15 20:52:23 -0300

    aula 13

---
+++ js_completo/aula13/js/aula13.js
@@ -0,0 +1,15 @@
+var item1 = 10;
+var item2 = 10;
+
+var soma = item1 + item2;
+console.log(soma);
+var outraSoma = 10 + 10;
+console.log(outraSoma);
+var soma1 = (10 + 10) / 2;
+console.log(soma1);
+var teste = 10;
+console.log(teste++);
+var teste1 = 10;
+console.log(++teste1);
+var item3 = (item1/item2);
+console.log(item3);
