f39c9cef9f80c404aee935fd05fdd2de0ac90f76
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-15 20:43:29 -0300

    aula 12

---
+++ js_completo/aula12/js/aula12.js
@@ -0,0 +1,9 @@
+var array = ['Keven', 24, 'São Paulo', {nome: 'Mundão'}];
+
+console.log(array);
+console.log(array.length);
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[3].nome);
