7c07356a82e25b5429151525aea04d595a813275
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-14 21:06:07 -0300

    aula 09

---
+++ js_completo/aula09/js/aula09.js
@@ -0,0 +1,5 @@
+console.log(document.querySelector('.elementaryos'));
+console.log(document.querySelector('.elementaryos').parentNode);
+console.log(document.querySelector('.elementaryos').parentNode.parentNode);
+console.log(document.querySelector('.debian'));
+console.log(document.querySelector('.debian').parentNode);
