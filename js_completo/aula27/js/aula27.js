da11bac971e34711d61574cbe3a6be14c5529b9a
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-07-21 20:44:58 -0300

    aula 27

---
+++ js_completo/aula27/js/aula27.js
@@ -0,0 +1,14 @@
+var box = $('.box');
+
+box.animate({
+    width: '200px',
+    height: '200px',
+    left: '500px'
+}, 5000)
+
+/*
+box.show();
+box.hide();
+box.fadeIn(3000);
+box.fadeOut(3000);
+*/
