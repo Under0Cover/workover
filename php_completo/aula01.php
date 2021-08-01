ca0151bf7a10a3efef1d89775a13b94020ed253d
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-08-01 13:31:00 -0300

    aula 01

---
+++ php_completo/aula01.php
@@ -0,0 +1,20 @@
+<?php
+    // Comentário Simples em PHP
+    echo("<font color=#ff00000>Primeira linha em PHP</font><br/>");
+    echo("Curso de PHP");
+    /* 
+        Comentário de bloco em PHP
+    */
+?>
+
+<html>
+    <head>
+        <title>Aula 1 de PHP</title>
+    </head>
+    <body>
+        <p>Texto escrito em HTML!</p>
+        <?php
+            echo("Texto escrito em PHP!");
+        ?>
+    </body>
+</html>
