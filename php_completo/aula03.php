8b65ed8f7ecfcb8e3f89580857d824ba099d283f
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-08-02 22:40:26 -0300

    aula 03

---
+++ php_completo/aula03.php
@@ -0,0 +1,44 @@
+<?php
+
+    $numeroUm = 10;
+    $numeroDois = 5;
+
+    //Aritméticos (Matemáticos)
+    $resultado = $numeroUm + $numeroDois;
+    echo("A soma de $numeroUm com $numeroDois = $resultado <br/>");
+    $resultado = $numeroUm - $numeroDois;
+    echo("A subtração de $numeroUm com $numeroDois = $resultado<br/>");
+    $resultado = $numeroUm / $numeroDois;
+    echo("A divisão de $numeroUm e $numeroDois = $resultado<br/>");
+    $resultado = $numeroUm % $numeroDois;
+    echo("O resto da divisão entre $numeroUm e $numeroDois = $resultado<br/>");
+    $resultado = $numeroUm * $numeroDois;
+    echo("A multiplicação de $numeroUm e $numeroDois = $resultado<br/>");
+    echo("<hr>");
+
+
+    //Operadores de Atribuição
+    $numeroUm++;
+    echo("Novo valor de número um: $numeroUm<br/>");
+    $numeroDois += 2;
+    echo("Novo valor de número dois: $numeroDois<br/>");
+    echo("<hr>");
+
+    //Operadores de Comparação
+    // == != > < >= <= 
+    // AND OR XOR   
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 3 de PHP - Operações com Variáveis</title>
+</head>
+<body>
+    
+</body>
+</html>
