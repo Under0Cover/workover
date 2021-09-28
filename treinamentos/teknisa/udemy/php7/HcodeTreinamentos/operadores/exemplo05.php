820aa90a0fe18b77cc26de622aaf97ac7498e047
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-09-28 19:42:39 -0300

    comparador spaceship

---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/operadores/exemplo05.php
@@ -0,0 +1,15 @@
+<?php
+    //Spaceship
+    $valor1 = 50;
+    $valor2 = 35;
+
+    var_dump($valor1 <=> $valor2);
+    /*
+        Spaceship são as 3 comparações ao mesmo tempo.
+        Considere (a <=> b), caso
+        "a" seja maior, o retorno será 1;
+        amobs sejam iguais, o retorno será 0;
+        "b" seja maior, o retorno será -1.
+    */
+
+?>
