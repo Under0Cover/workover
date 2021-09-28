18cb9ff0646680a8069fdb73219ed77b56aaf458
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-09-28 14:19:37 -0300

    operadores aritmeticos

---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/operadores/exemplo02.php
@@ -0,0 +1,14 @@
+<?php
+    // Simulando como se fosse uma compra em um site
+    $valorTotal = 0;
+
+    $valorTotal += 100;
+    $valorTotal += 25;
+
+    echo $valorTotal;
+    echo '<hr>';
+
+    // Simulando um cupom de desconto
+    echo $valorTotal *= .9;
+
+?>
---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/operadores/exemplo03.php
@@ -0,0 +1,25 @@
+<?php
+
+    //Operadores Aritméticos
+    $valor1 = 10;
+    $valor2 = 2;
+
+    echo $valor1 + $valor2;
+    echo '<hr>';
+
+    echo $valor1 - $valor2;
+    echo '<hr>';
+
+    echo $valor1 * $valor2;
+    echo '<hr>';
+
+    echo $valor1 / $valor2;
+    echo '<hr>';
+
+    echo $valor1 % $valor2;
+    echo '<hr>';
+
+    echo $valor1 ** $valor2;
+    
+
+?>
