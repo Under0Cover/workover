e8400df742353f144fe6ddb144d6f90abe5e138c
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-09-28 19:51:37 -0300

    incremento e decremento

---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/operadores/exemplo07.php
@@ -0,0 +1,20 @@
+<?php
+
+    // Operador de incremento e decremento
+
+    $valor1 = 10;
+
+    echo $valor1++;
+    echo '<hr>';
+    echo $valor1;
+    echo '<br>';
+    echo ++$valor1;
+    echo '<hr>';
+    echo $valor1--;
+    echo '<hr>';
+    echo $valor1;
+    echo '<br>';
+    echo --$valor1;
+    echo '<hr>';
+
+?>
