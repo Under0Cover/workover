3c9d97f80d16c4c69537179c2de9b6a0efee30e2
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-09-28 14:12:58 -0300

    operadores de string

---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/operadores/exemplo01.php
@@ -0,0 +1,14 @@
+<?php
+    //Operadores de String
+    // Atribuição '='
+    $nome = 'Hcode';
+
+    // Concatenação '.'
+    echo $nome.' mais alguma coisa';
+    echo '<hr>';
+
+    // Composto '.='
+    $nome .= ' Treinamentos';
+    echo $nome;
+    
+?>
