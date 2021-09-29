f3d35932ff7ebd8c6043fabd8039df3dacb3433b
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-09-29 20:43:00 -0300

    testes include

---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/testes_exercicios/testes_include/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    require 'server.php';
+    require 'config.php';
+
+    /*
+        O objetivo do exercício é ocasionar o erro de repetições de 'require' e pedir a solução
+    */
+
+    // Resposta: Utilizar o require_once ao invés do require
+
+?>
---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/testes_exercicios/testes_include/server.php
@@ -0,0 +1,8 @@
+<?php
+
+    function connect() {
+        $server = '127.0.0.1';
+        return $server;
+    }
+
+?>
