547564f66c40d3fb1f0f7c418468a67637afa915
Tales Oliver <talis.oliveira@hotmail.com.br>
2021-10-04 21:36:40 -0300

    arrays Parte 1

---
+++ treinamentos/teknisa/udemy/php7/HcodeTreinamentos/arrays/exemplo01.php
@@ -0,0 +1,14 @@
+<?php
+
+    $frutas = array('Laranja', 'Abacaxi', 'Melancia');
+
+    print_r($frutas);
+    /*
+        Regras do Array
+        1) Todo Array inicia-se em 0
+        2) Quando o Array só tem uma direção, ele é chamado de VETOR
+            No caso de $frutas, ele não é um Array propriamente dito, é um vetor.
+            Para ser um Array, propriamente dito, ele precisa ter mais de uma dimensão.
+    */
+
+?>
